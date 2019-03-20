9eeb144b2c85e1e9084da471764d9464e84fe900
Suman Chatterjee <sum_chat@hotmail.com>
2019-03-19 22:47:44 -0400

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ const STATUS_CODE_LATE_AIRLINE = 20;
 const STATUS_CODE_LATE_WEATHER = 30;
 const STATUS_CODE_LATE_TECHNICAL = 40;
 const STATUS_CODE_LATE_OTHER = 50;
-const ORACLES_COUNT = 20; 
+const ORACLES_COUNT = 40; 
 const STATUSCODES  = [STATUS_CODE_UNKNOWN, STATUS_CODE_ON_TIME, STATUS_CODE_LATE_AIRLINE, STATUS_CODE_LATE_WEATHER, STATUS_CODE_LATE_TECHNICAL, STATUS_CODE_LATE_OTHER];
   // Track all registered oracles
  let oracles= {};
@@ -22,41 +22,39 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 let flightSuretyData = new web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
 
-web3.eth.getAccounts((error, accounts) => {
+
+web3.eth.getAccounts().then((accounts) => { 
+  console.log("length :"+ accounts.length);
   flightSuretyData.methods.authorizeCaller(config.appAddress)
-                          .send({ from: accounts[0] }, (error, result) => {
-                         if(error) {
-                              console.log(error);
-                         } else {
-                            console.log("registered appAddress as the authorized contract of dataContract");
-                             }
-                     });
-             });
-// ARRANGE
-
-/* const registerOracles = async() => {
-
-
-let fee = await flightSuretyApp.REGISTRATION_FEE.call();
-
-     web3.eth.getAccounts((error, accounts) => {
-        for(let a=12; a<ORACLES_COUNT; a++) { 
-          try {
-              flightSuretyApp.methods.registerOracle({ from: accounts[a], value: fee });             
-              let result = flightSuretyApp.methods.getMyIndexes({from: accounts[a]});
-              oracles[accounts[a]] = result;
-              console.log("registered oracle with address:" + accounts[a]);
-              } 
-          catch(e) {
-              console.log('Error while registering oracle with address:' + accounts[a] );
-              }     
+     .send({from: accounts[0]})
+     .then(result => {
+      console.log("appAddress registered as the authorized contract of dataContract");
+    })
+    .catch(error => {
+      console.log("Error in authorizing appcontract. " + error);
+    });
+     flightSuretyApp.methods.REGISTRATION_FEE().call().then(fee => {
+      for(let a=12; a<ORACLES_COUNT; a++) {
+        flightSuretyApp.methods.registerOracle()
+        .send({ from: accounts[a], value: fee,gas:4000000 })
+        .then(result=>{
+          flightSuretyApp.methods.getMyIndexes().call({from: accounts[a]})
+          .then(indices =>{
+            oracles[accounts[a]] = indices;
+            console.log("Oracle registered: " + accounts[a] + " indices:" + indices);
+          })
+        }) 
+        .catch(error => {
+          console.log("Error while registering oracles: " + accounts[a] +  " Error: " + error);
+        });           
+      }
+     })  
     
-        }
-      })
-    } */
+
+});
 
 
-//registerOracles();
+
 
 flightSuretyApp.events.OracleRequest({
     fromBlock: 0
@@ -66,23 +64,23 @@ flightSuretyApp.events.OracleRequest({
       const index = event.returnValues.index;
       const airline = event.returnValues.airline;
       const flight = event.returnValues.flight;
-      const timestamp = event.returnValues.timestamp;
-      
+      const timestamp = event.returnValues.timestamp;      
 
       for(var key in oracles)
       {
         var indexes = oracles[key];
         if(indexes.includes(index))
-        {
-          try{
+        {          
           let randomstatusCode = STATUSCODES[Math.floor(Math.random()*STATUSCODES.length)];
-          flightSuretyApp.methods.submitOracleResponse(index, airline, flight, timestamp, randomstatusCode,{ from: key});
-          console.log("statuscode:" + randomstatusCode + " for "+ accounts[a])
-          }
-          catch(e) {
-            console.log('Error while submitting oracle response:' + accounts[a] );
-            }  
-
+          flightSuretyApp.methods.submitOracleResponse(index, airline, flight, timestamp, randomstatusCode)
+          .send({ from: key})
+          .then(result =>{
+            console.log("Oracle response sent with statuscode:" + randomstatusCode + " for "+ flight)
+          })
+          .catch(error =>{
+            console.log("Error while sending Oracle response  for "+ flight)
+          });          
+           
         }
       }
       console.log(event);
